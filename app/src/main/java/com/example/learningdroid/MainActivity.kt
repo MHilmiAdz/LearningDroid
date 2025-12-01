f783d372199839e935db68ea1c3cbd02aa585409
mhilm <m.hilmi.adz@gmail.com>
2025-12-01 09:53:28 +0700

    ADD | Move to another page using intent

--- app/src/main/java/com/example/learningdroid/MainActivity.kt
+++ app/src/main/java/com/example/learningdroid/MainActivity.kt
@@ -1,51 +1,27 @@
 package com.example.learningdroid
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var edtWidth: EditText
-    lateinit var edtHeight: EditText
-    lateinit var edtLength: EditText
-    lateinit var btnCalculate: Button
-    lateinit var tvResult: TextView
-
-    companion object {
-        private const val STATE_RESULT = "state_result"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (savedInstanceState != null) {
-            val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
-        }
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
-        btnCalculate.setOnClickListener(this)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
     }
 
-    override fun onClick(view: View?) {
-        if (view?.id == R.id.btn_calculate) {
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
-            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-            tvResult.text = volume.toString()
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
     }
 }
